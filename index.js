5c348e431e4f03e04d6135ec0f95788a38ec2290
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-06-27 01:55:00 -0400

    created third table orders solving issue #1

--- index.js
+++ index.js
@@ -13,17 +13,24 @@ const pool = new Pool ({
 //Creating Tables
 
 //create 'customers' table
-pool.query("CREATE TABLE customers(customer_id BIGSERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL, email VARCHAR(150) NOT NULL, phone_number VARCHAR(50) NOT NULL)", (error, result) => {
-    console.log(error, result)
-    //pool.end()
-})
-//create 'books' table
-pool.query("CREATE TABLE books(product_id BIGSERIAL PRIMARY KEY, book_name VARCHAR(50) NOT NULL, description VARCHAR(150) NOT NULL, price DEC(4,2) NOT NULL, quatity INT NOT NULL)", (error, result) => {
-    console.log(error, result)
-    pool.end()
-})
+// pool.query("CREATE TABLE customers(customer_id BIGSERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL, email VARCHAR(150) NOT NULL, phone_number VARCHAR(50) NOT NULL)", (error, result) => {
+//     console.log(error, result)
+//     //pool.end()
+// })
+// //create 'books' table
+// pool.query("CREATE TABLE books(product_id BIGSERIAL PRIMARY KEY, book_name VARCHAR(50) NOT NULL, description VARCHAR(150) NOT NULL, price DEC(4,2) NOT NULL, quatity INT NOT NULL)", (error, result) => {
+//     console.log(error, result)
+//     //pool.end()
+// })
 //create 'orders' table
+pool.query("CREATE TABLE orders(order_id BIGSERIAL PRIMARY KEY, customer_id BIGSERIAL REFERENCES customers(customer_id), order_date TIMESTAMP NOT NULL, total_amount DECIMAL(4,2) NOT NULL)", (error, result) => {
+    console.log(error, result);
+    pool.end();
+  });  
 //create 'order_items' table
+// pool.query("CREATE TABLE order_items(ordered_books_id BIGSERIAL PRIMARY KEY, order_id BIGSERIAL FOREIGN KEY NOT NULL, book_id BIGSERIAL FOREIGN KEY NOT NULL, quantity INT NOT NULL, price DEC(4,2) NOT NULL)", (error, result) => {
+//     console.log(error, result)
+// })
 
 
 

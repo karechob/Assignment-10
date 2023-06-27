73b550de83c78ab34103c96a99a8d219ae70b2a8
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-06-27 02:03:08 -0400

    error w format

--- index.js
+++ index.js
@@ -11,27 +11,37 @@ const pool = new Pool ({
 })
 
 //Creating Tables
+//each table was created on a separe node index.js run
 
-//create 'customers' table
+// create 'customers' table
+
 // pool.query("CREATE TABLE customers(customer_id BIGSERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL, email VARCHAR(150) NOT NULL, phone_number VARCHAR(50) NOT NULL)", (error, result) => {
 //     console.log(error, result)
-//     //pool.end()
+//     pool.end()
 // })
-// //create 'books' table
+
+// create 'books' table
+
 // pool.query("CREATE TABLE books(product_id BIGSERIAL PRIMARY KEY, book_name VARCHAR(50) NOT NULL, description VARCHAR(150) NOT NULL, price DEC(4,2) NOT NULL, quatity INT NOT NULL)", (error, result) => {
 //     console.log(error, result)
-//     //pool.end()
+//     pool.end()
 // })
-//create 'orders' table
+
+// create 'orders' table
+
 // pool.query("CREATE TABLE orders(order_id BIGSERIAL PRIMARY KEY, customer_id BIGSERIAL REFERENCES customers(customer_id), order_date TIMESTAMP NOT NULL, total_amount DECIMAL(4,2) NOT NULL)", (error, result) => {
 //     console.log(error, result);
 //     pool.end();
 //   });  
-//create 'order_items' table
-pool.query("CREATE TABLE order_items(ordered_books_id BIGSERIAL PRIMARY KEY, order_id BIGINT NOT NULL, book_id BIGINT NOT NULL, quantity INT NOT NULL, price DECIMAL(4,2) NOT NULL, FOREIGN KEY (order_id) REFERENCES orders(order_id), FOREIGN KEY (book_id) REFERENCES books(product_id))", (error, result) => {
-    console.log(error, result);
-    pool.end();
-  });
+
+// create 'order_items' table
+
+// pool.query("CREATE TABLE order_items(ordered_books_id BIGSERIAL PRIMARY KEY, order_id BIGINT NOT NULL, book_id BIGINT NOT NULL, quantity INT NOT NULL, price DECIMAL(4,2) NOT NULL, FOREIGN KEY (order_id) REFERENCES orders(order_id), FOREIGN KEY (book_id) REFERENCES books(product_id))", (error, result) => {
+//     console.log(error, result);
+//     pool.end();
+//   });
+
+//Inserting values into table
   
 
 
